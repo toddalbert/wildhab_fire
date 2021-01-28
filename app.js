554f8b0b386dbbc4803738c9ec9a27667748a71f
Rebecca Bakels <rebeccabakels@gmail.com>
2021-01-28 14:55:33 -0500

    added updatePerson

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { getEvents, postEvent } = require('./src/events')
 const { getPeople, postPerson } = require('./src/users')
-const { getPerson } = require('./src/users/users.js')
+const { getPerson, updatePerson } = require('./src/users/users.js')
 const { getSingleEvent, deleteEvent, updateEvent } = require('./src/events/eventId')
 
 const app = express()
@@ -18,6 +18,7 @@ app.patch('/events/:eventId', updateEvent)
 app.get('/people', getPeople)
 app.post('/people', postPerson)
 app.get('/person/:personId', getPerson)
+app.patch('/person/:personId', updatePerson)
 
 app.get('/', (req, res) => {
 	res.status(200).send('Hello World!')
